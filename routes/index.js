83734e34feaaa4f975946e45e6c88c7b60a33e21
Jason Boyce <jasonboyce75@gmail.com>
2014-11-17 13:01:11 -0700

    added support for nested subfolders

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,44 @@
+"use strict";
+
 var fs = require("fs");
 var common = require("../common");
+var walkdir = require("walkdir");
 
-var getMp3Paths = function() {
-    var fileNames = fs.readdirSync(common.audioPath);
+var getMp3PathInfos = function() {
+    var roothAudioPathLength = fs.realpathSync(common.audioPath).length;
+    var pathMap = {};
+    var mainPath = "main";
 
-    return fileNames.filter(function(name) {
-        return name.endsWith(".mp3");
+    var paths = walkdir.sync(common.audioPath);
+    paths.forEach(function(path) {
+        if (path.endsWith(".mp3")) {
+            var relativePath = path.substring(roothAudioPathLength);
+            var separator = relativePath[0];
+            var pathParts = relativePath.split(separator);
+            var fileName = pathParts[pathParts.length - 1];
+            pathParts.pop();
+            pathParts.shift(); //remove the first element since it is empty because of the leading separator in the path
+            var key;
+            if (pathParts.length == 0) {
+                key = mainPath;
+            }
+            else {
+                key = pathParts.join(separator);
+            }
+
+            pathMap[key] = pathMap[key] || [];
+            var folder = (key === mainPath) ? "" : key + separator;
+            pathMap[key].push({
+                text: fileName.substring(0, fileName.length - 4),
+                href: '/play/' + folder + encodeURIComponent(fileName).replace(/'/g, "%27")
+            });
+        }
     });
+
+    return pathMap;
 };
 
 exports.index = function(request, response){
-    var pathsWithLinks = [];
-    var mp3Paths = getMp3Paths();
-
-    mp3Paths.forEach(function (path) {
-        var pathWithLink = {
-            text: path.substring(0, path.length - 4),
-            href: '/play/' + encodeURIComponent(path).replace(/'/g, "%27")
-        };
-        pathsWithLinks.push(pathWithLink);
-    });
-
-  response.render('index', { mp3Paths : pathsWithLinks });
+    var mp3PathInfos = getMp3PathInfos();
+    response.render('index', { keys: Object.getOwnPropertyNames(mp3PathInfos), mp3Paths : mp3PathInfos });
 };
